c8678d89288d2a5c7ed828c5a563ac1e08cc4c3f
Ramon Erik <ramonerik29a@gmail.com>
2023-01-18 13:50:47 -0300

    Update script.js
    added the witted way of the hours and the base where it was gotten (the capitals)

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const ponteiroHoras = document.querySelector('.horas')
 const ponteiroMinutos = document.querySelector('.minutos')
 const ponteiroSegundos = document.querySelector('.segundos')
+const textHoras = document.querySelector('.texto-horas')
 
 function definirRegiao() {
     let local = document.querySelector('#regiao')
     let regiao = local.options[local.selectedIndex].value
     let fuso = 0
+    let base = 0
     switch (regiao) {
         case 'brasil':
             fuso = -3
+            base = 'Brasília, DF'
             break
-        default:
+        case 'mexico':
+            fuso = -6
+            base = 'Distrito Federal, México'
+            break
+        case 'eua':
             fuso = -5
+            base = 'Washington, D.C'
+            break
+        case 'china':
+            fuso = 8
+            base = 'China'
             break
     }
-    let valores = [regiao, fuso]
+    let valores = [regiao, fuso, base]
     return valores
 }
 
@@ -25,23 +37,29 @@ function atualizarHora() {
     const sec = horaAtual.getSeconds()
     
     let diferencaHorario = definirRegiao()[1] - (horaAtual.getTimezoneOffset()/-60)
-    console.log(diferencaHorario)
 
     if (horaAtual.getTimezoneOffset()/-60 < 0) {
         hora += diferencaHorario
     } else {
         hora -= diferencaHorario
     }
-
-    console.log(hora)
-
+    
     const anguloHora = hora / 12 *360
     const anguloMin = min / 60 * 360
     const anguloSec = sec / 60 * 360
-
+    
     ponteiroHoras.style.transform = `rotate(${anguloHora}deg)`
     ponteiroMinutos.style.transform = `rotate(${anguloMin}deg)`
     ponteiroSegundos.style.transform = `rotate(${anguloSec}deg)`
+
+    if (hora == 24) {
+        hora = '00'
+    }
+    if (min < 10) {
+        min = `0${min}`
+    }
+    textHoras.innerText = `São ${hora}:${min} Horário em ${definirRegiao()[2]} (GMT${definirRegiao()[1]})`
+    // console.log(horaAtual)
 }
 
 setInterval(atualizarHora, 1000)
