2cffbcb17b5dd6d42d92dffa5c57eb46bcdad02d
Ramon Erik <ramonerik29a@gmail.com>
2023-01-18 09:02:31 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const ponteiroHoras = document.querySelector('.horas')
+const ponteiroMinutos = document.querySelector('.minutos')
+const ponteiroSegundos = document.querySelector('.segundos')
+
+function atualizarHora() {
+    const horaAtual = new Date()
+    console.log(horaAtual)
+}
+
+atualizarHora()
